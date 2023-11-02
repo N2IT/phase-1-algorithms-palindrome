01d49a3e19078d0f62e80cda6285b5c94e52347a
Tony Eder <tonyeder11@gmail.com>
2023-11-02 14:49:32 -0700

    pretty cool stuff here!

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let drow = word.split("").reverse().join("")
+  return word === drow ? true : false
 }
 
 /* 
   Add your pseudocode here
-*/
+  //define palindrome
+*/ //take argument and pass to if / else statement to verify if argument is palindrome or not
 
 /*
   Add written explanation of your solution here
@@ -15,6 +18,7 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("beavis"))
 
   console.log("");
 
